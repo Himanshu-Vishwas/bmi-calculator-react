7bed8b6ab6b69152297fbcee3662183fa1d7d4ad
Himanshu-vishwas <himanshuvishwas58@gmail.com>
2022-12-04 03:09:01 +0530

    with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Changes to be committed:
    	deleted:    src/App.css
    	modified:   src/App.js
    	deleted:    src/App.test.js
    	modified:   src/index.css
    	modified:   src/index.js
    	deleted:    src/logo.svg
    	deleted:    src/reportWebVitals.js
    	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import './index.css'
 
 function App() {
+
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+
+
+  let imgSrc = ''
+
+
+  let calcBmi = (event) =>{
+    event.preventDefault()
+
+    if(weight===0 || height === 0){
+      alert('Enter valid data...')
+    }else{
+      let bmi = (weight/(height*height)*703)
+      setBmi(bmi.toFixed(1))
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h2 className="center">BMI Calculator</h2>
+        <form>
+          <div>
+            <label>Weight (lbs) </label>
+            <input value={weight} onChange={(event) => setWeight(event.target.value )}/>
+          </div>
+          <div>
+            <label>Height (in) </label>
+            <input value={height} onChange={(event)=>setHeight(event.target.value)}/>
+          </div>
+          <div>
+            <button className="btn" type="submit">Submit</button>
+            <button className="btn btn-outline" type="submit">Submit</button>
+          </div>
+        </form>
+
+        <div className="center">
+          <h3>Your BBMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+        <div className="img-container">
+          <img src = {imgSrc} alt=''></img>
+        </div>
+        </div>
     </div>
   );
 }
