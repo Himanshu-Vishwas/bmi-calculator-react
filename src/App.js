24cbd955205d9bf4a15919a3e96557d4f23c9168
Himanshu-vishwas <himanshuvishwas58@gmail.com>
2022-12-04 03:32:35 +0530

    On branch master Changes to be committed: 	modified:   src/App.js 	new file:   src/assets/healthy.png 	new file:   src/assets/overweight.png 	new file:   src/assets/under.png

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
 
 
 
-  let imgSrc = ''
-
-
   let calcBmi = (event) =>{
     event.preventDefault()
 
@@ -22,16 +19,40 @@ function App() {
     }else{
       let bmi = (weight/(height*height)*703)
       setBmi(bmi.toFixed(1))
+
+      if(bmi<25){
+        setMessage('You are underweight.')
+      }else if(bmi>=25 && bmi<30){
+        setMessage('You are healthy.')
+      }else{ 
+        setMessage('You are overweight.')
+      }
+
     }
   }
 
+  let imgSrc;
+  if(bmi<1){
+    imgSrc = null
+  }else{
+    if(bmi<25){
+      imgSrc = require('../src/assets/under.png')
+    }else if(bmi>=25 && bmi<30){
+      imgSrc = require('../src/assets/healthy.png')
+    }else{
+      imgSrc = require('../src/assets/overweight.png')
+    }
+  }
 
+  let reset = () =>{
+    window.location.reload()
+  }
 
   return (
     <div className="app">
       <div className="container">
         <h2 className="center">BMI Calculator</h2>
-        <form>
+        <form onSubmit={calcBmi}>
           <div>
             <label>Weight (lbs) </label>
             <input value={weight} onChange={(event) => setWeight(event.target.value )}/>
@@ -42,12 +63,12 @@ function App() {
           </div>
           <div>
             <button className="btn" type="submit">Submit</button>
-            <button className="btn btn-outline" type="submit">Submit</button>
+            <button className="btn btn-outline" onClick={reset} type="submit">Reset</button>
           </div>
         </form>
 
         <div className="center">
-          <h3>Your BBMI is: {bmi}</h3>
+          <h3>Your BMI is: {bmi}</h3>
           <p>{message}</p>
         </div>
         <div className="img-container">
